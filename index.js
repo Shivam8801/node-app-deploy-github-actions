6776571fa7c618a747e9c48dca5a136e900c70cf
Shivam8801 <shivammh15@gmail.com>
2026-04-20 20:16:31 +0530

    fix ssh

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT ?? 8080;
 
 app.get('/', (req, res) => {
-    res.json({ message: 'Hello from the server!' });
+    res.json({ message: 'Hello from the ubuntu server!' });
 });
 
 
