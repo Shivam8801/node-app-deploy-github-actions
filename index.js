5cca47c21470eeeb48b048b2657c4f1ae4eeaae8
Shivam8801 <shivammh15@gmail.com>
2026-04-20 20:27:27 +0530

    test ci cd

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT ?? 8080;
 
 app.get('/', (req, res) => {
-    res.json({ message: 'Hello from ubuntu server!' });
+    res.json({ message: 'Hello from the ubuntu server!' });
 });
 
 
